cca30f20712bab40cdc49968e11c1f385188d2a6
Soham Kolhatkar <sohamk130405@gmail.com>
2025-02-18 03:39:09 +0530

    added video in hero section

--- app/(marketing)/page.jsx
+++ app/(marketing)/page.jsx
@@ -9,6 +9,7 @@ import { Button } from "@/components/ui/button";
 
 import MagicBadge from "@/components/ui/magic-badge";
 import MagicCard from "@/components/ui/magic-card";
+import VideoPlayer from "@/components/video-player";
 import { COMPANIES, PROCESS } from "@/utils/constants/misc";
 import { ArrowRightIcon } from "lucide-react";
 import Image from "next/image";
@@ -63,18 +64,11 @@ const HomePage = async () => {
             className="relative pt-20 pb-20 md:py-32 px-2 bg-transparent w-full"
           >
             <div className="absolute md:top-[10%] left-1/2 gradient w-3/4 -translate-x-1/2 h-1/4 md:h-1/3 inset-0 blur-[5rem] animate-image-glow"></div>
-            <div className="-m-2 rounded-xl p-2 ring-1 ring-inset ring-foreground/20 lg:-m-4 lg:rounded-2xl bg-opacity-50 backdrop-blur-3xl">
+            <div className="-m-2 rounded-xl p-2 ring-1 ring-inset ring-foreground/20 lg:-m-4 lg:rounded-2xl bg-opacity-50 backdrop-blur-3xl relative">
               <BorderBeam size={250} duration={12} delay={9} />
-              <Image
-                src="/assets/thumbnail.png"
-                alt="Dashboard"
-                width={1200}
-                height={1200}
-                quality={100}
-                className="rounded-md lg:rounded-xl bg-foreground/10 ring-1 ring-border"
-              />
-              <div className="absolute -bottom-4 inset-x-0 w-full h-1/2 bg-gradient-to-t from-background z-40"></div>
-              <div className="absolute bottom-0 md:-bottom-8 inset-x-0 w-full h-1/4 bg-gradient-to-t from-background z-50"></div>
+              <VideoPlayer videoSrc="/assets/edge.mp4" />
+              <div className="absolute -bottom-10 inset-x-0 w-full h-1/2 bg-gradient-to-t from-background z-10"></div>
+              <div className="absolute bottom-0 md:-bottom-8 inset-x-0 w-full h-1/4 bg-gradient-to-t from-background z-20"></div>
             </div>
           </AnimationContainer>
         </div>
